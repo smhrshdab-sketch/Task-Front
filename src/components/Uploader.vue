ce651a2224ce3c8fb222e81a88a913a2a7e0b024
Soroosh <s.mhrshd.ab@gmail.com>
2026-07-10 01:27:57 +0330

    add drag and drop to the component

--- src/components/Uploader.vue
+++ src/components/Uploader.vue
@@ -12,6 +12,7 @@
     const fileData = ref<Attachment | null>(null)
     const attachmentList = ref<Attachment[]>([])
     const attachedFiles = ref<File[]>([]) 
+    const isDragging = ref<boolean>(false)
 
     const triggerUpload = () => {
         console.log(fileInput.value)
@@ -43,17 +44,61 @@
             }
         })
         console.info('Selected files:', attachmentList.value)
+        input.value = ''
     }
-    const handleDroppedFile = () => {
-        //
+    const handleDroppedFile = (event: Event) => {
+        const input = event.target as HTMLInputElement
+        //event.dataTransfer?.files        
+        if (!input.files) return
+    }
+    const handleDragOver = (event: DragEvent) => {
+        event.preventDefault() //prevent browser open or play the fileS
+        isDragging.value = true
+    }
+
+    const handleDragLeave = () => {
+         isDragging.value = false
+    }
+
+    const handleDrop = (event: DragEvent) => {
+        event.preventDefault() //prevent browser open or play the fileS
+        isDragging.value = false
+    
+        const files = event.dataTransfer?.files
+        attachedFiles.value.push(...Array.from(files))
+        attachedFiles.value.forEach(file => {
+            console.log('Processing file:', file.name)        
+            if (file) {
+                    if (file.size > 25 * 1024 * 1024) {
+                    alert("File size have to less than 25MB")
+                    return
+                }
+
+                fileData.value = {
+                    name: file.name,
+                    size: file.size,
+                    type: file.type,
+                    content: file
+                }
+                attachmentList.value.push(fileData.value)
+                console.info('File selected:', fileData.value)
+            }
+        })
+        console.info('Selected files:', attachmentList.value)
     }
 </script>
 
 <template>
   <div 
     @click="triggerUpload"
-    @dragover.prevent
-    class="cursor-pointer max-w-96 p-6 bg-[#e3e3e3] flex flex-col justify-center items-center border-4 border-dashed border-gray-500 rounded-2xl shadow-2xl hover:border-green-500 hover:bg-[#b7b7b7] transition-all duration-200"
+    @dragover="handleDragOver"
+    @dragleave="handleDragLeave"
+    @drop="handleDrop"
+    :class="[
+      'cursor-pointer max-w-96 p-6 flex flex-col justify-center items-center border-2 border-dashed rounded-2xl shadow-2xl transition-all duration-200',
+      isDragging ? 'border-green-500 bg-[#b7b7b7]' : 'border-gray-500 bg-[#e3e3e3]',
+      fileData ? 'hover:border-red-500' : 'hover:border-green-500 hover:bg-[#b7b7b7]' //delete selection
+    ]"
   >
     <img class="w-16 h-16" src="@/assets/upload.png" alt="upload icon">
     <span class="font-bold">Click to upload</span>
@@ -67,4 +112,17 @@
       multiple
     >
   </div>
+  <div 
+        v-if="attachmentList"
+        class="flex flex-col gap-2 bg-blue-100 shadow-2xl P-2" 
+        v-for="(attach,index) in attachmentList" 
+        :key="index"
+    >
+        <div v-if="attach" class="flex flex-row items-center p-2 gap-2 border-2 rounded-2xl">
+            <p class="text-center font-medium break-all">{{ attach.name }}</p>
+            <p class="text-sm text-gray-500">{{ (attach.size / 1024 / 1024).toFixed(2) }} MB</p>
+
+            <button @click.stop="attach = null" class="mt-3 px-3 py-1 bg-red-500 text-white rounded hover:bg-red-600 transition duration-200">Remove</button>
+        </div>
+    </div>   
 </template>
