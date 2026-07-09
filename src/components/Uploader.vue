86a8f5b1a403f0e11fd1c5e86e8c4acba5c1b92e
Soroosh <s.mhrshd.ab@gmail.com>
2026-07-09 19:05:45 +0330

    remove some unnesessurey  explanations + add multiple(file) property to the component

--- src/components/Uploader.vue
+++ src/components/Uploader.vue
@@ -8,8 +8,10 @@
         content: File
     }
 
-    const fileInput = ref<HTMLInputElement | null>(null) // رفرنس به اینپوت
+    const fileInput = ref<HTMLInputElement | null>(null) 
     const fileData = ref<Attachment | null>(null)
+    const attachmentList = ref<Attachment[]>([])
+    const attachedFiles = ref<File[]>([]) 
 
     const triggerUpload = () => {
         console.log(fileInput.value)
@@ -18,59 +20,51 @@
 
     const handleFileChange = (event: Event) => {
         const input = event.target as HTMLInputElement
-        const file = input.files?.[0]
-        
-        if (file) {
-                if (file.size > 25 * 1024 * 1024) {
-                alert("حجم فایل نباید بیشتر از 25 مگابایت باشد")
-                return
-            }
+        //const file = input.files?.[0]
+        if (!input.files) return
+        //const filesArray = Array.from(input.files)
+        attachedFiles.value.push(...Array.from(input.files))
+        attachedFiles.value.forEach(file => {
+            console.log('Processing file:', file.name)        
+            if (file) {
+                    if (file.size > 25 * 1024 * 1024) {
+                    alert("File size have to less than 25MB")
+                    return
+                }
 
-            // مقداردهی شیء
-            fileData.value = {
-                name: file.name,
-                size: file.size,
-                type: file.type,
-                content: file
+                fileData.value = {
+                    name: file.name,
+                    size: file.size,
+                    type: file.type,
+                    content: file
+                }
+                attachmentList.value.push(fileData.value)
+                console.info('File selected:', fileData.value)
             }
-            console.info('File selected:', fileData.value)
-         }
-         //=======
-         const file2 = input.files?.[1]
-        
-        if (file2) {
-                if (file2.size > 25 * 1024 * 1024) {
-                alert("حجم فایل نباید بیشتر از 25 مگابایت باشد")
-                return
-            }
-
-            // مقداردهی شیء
-            fileData.value = {
-                name: file2.name,
-                size: file2.size,
-                type: file2.type,
-                content: file2
-            }
-            console.info('File2 selected:', fileData.value)
-         }
+        })
+        console.info('Selected files:', attachmentList.value)
+    }
+    const handleDroppedFile = () => {
+        //
     }
 </script>
 
 <template>
   <div 
     @click="triggerUpload"
-    class="cursor-pointer max-w-96 p-6 bg-[#e3e3e3] flex flex-col justify-center items-center border-2 border-dashed border-gray-500 rounded-2xl shadow-2xl hover:border-green-500 hover:bg-[#b7b7b7] transition-all duration-200"
+    @dragover.prevent
+    class="cursor-pointer max-w-96 p-6 bg-[#e3e3e3] flex flex-col justify-center items-center border-4 border-dashed border-gray-500 rounded-2xl shadow-2xl hover:border-green-500 hover:bg-[#b7b7b7] transition-all duration-200"
   >
     <img class="w-16 h-16" src="@/assets/upload.png" alt="upload icon">
     <span class="font-bold">Click to upload</span>
     <span class="text-sm text-gray-600">Max file size: 25MB</span>
     
-    <!-- ref را اینجا اضافه کردیم -->
     <input 
       ref="fileInput" 
       type="file" 
       @change="handleFileChange" 
       class="hidden"
+      multiple
     >
   </div>
 </template>
