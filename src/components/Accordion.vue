7ef4cbae8fcb8e030a444db8795176657cc3c604
Soroosh <s.mhrshd.ab@gmail.com>
2026-07-31 16:33:24 +0330

    Write some testing components

---
+++ src/components/Accordion.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+    import {ref} from 'vue'
+    //====================
+    const props = defineProps<{
+        title:string,
+        content:string
+    }>()
+    const showContent = ref<boolean>(false)
+    const switchShowContent = () => {
+        showContent.value = !showContent.value
+    }
+</script>
+<template>
+    <div class="w-full min-h-16 m-2 p-2 flex justify-center rounded-xl border-gray-500 bg-yellow-100">
+        <div class="w-full flex flex-col gap-1 px-2">
+            <div class="w-full flex justify-between ">
+                <span class="text-xl font-medium hover:font-bold">{{ props.title }}</span>
+                <svg v-if="!showContent" @click="switchShowContent" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 hover:size-7">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
+                </svg>
+                <svg v-if="showContent" @click="switchShowContent" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 hover:size-7">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m4.5 15.75 7.5-7.5 7.5 7.5" />
+                </svg>
+            </div>
+            <div v-if="showContent" class="w-full bg-blue-100 p-2 rounded">{{ props.content }}</div>
+        </div>
+    </div>
+</template>
---
+++ src/components/Bar.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+    const props = defineProps<{
+        items:string[]
+    }>()
+</script>
+<template>
+    <ul class="m-2 min-w-64 py-1 px-3 flex justity-around rounded bg-gray-200">
+        <li 
+            class="p-2 text-xl hover:bg-gray-400 hover:font-semibold"
+            v-for="link in props.items" :key="link"
+        >            
+        {{ link }}
+        </li>
+    </ul>
+</template>
