3c2311479bfc03e8af7598440d6121d8f77c4cbc
Soroosh <s.mhrshd.ab@gmail.com>
2026-08-01 05:18:24 +0330

    remove some useless imports

--- src/views/task/TaskDetail.vue
+++ src/views/task/TaskDetail.vue
@@ -3,9 +3,8 @@
     import { useRoute, useRouter } from 'vue-router'
     import Accordion from '@/components/Accordion.vue'
     import axios from 'axios'
-    import api from '@/services/api.js'
-import MembershipDetail from '@/views/membership/MembershipDetail.vue';
-import Account from '@/views/Account.vue';
+    import api from '@/services/api.js';
+import Attachment from '@/Components/Shared/modals/AttachmentsModal/Attachment.vue';
     //=======================
     interface TaskDetail {
         department_id: string
@@ -35,7 +34,20 @@ import Account from '@/views/Account.vue';
                 description: string
                 created_at: string
                 updated_at: string
-            }
+            }            
+        }
+        department:{
+            id: number
+            title: string
+            description: string
+            status: string
+            created_at: string
+            updated_at: string
+        }
+        attachment:{
+            original_name:string
+            mime_type:string
+            size:number
         }
     }
     const route = useRoute()
@@ -88,6 +100,7 @@ import Account from '@/views/Account.vue';
     }
     onMounted(() => {
         fetchTask()
+        fetchEngages()
     })
 </script>
 <template>
@@ -175,7 +188,56 @@ import Account from '@/views/Account.vue';
                         </table>
                     </div>
                     <div v-else-if="flag == 'departments'">
-                        <p>Departments is clicked</p>
+                        <table class="w-full border-collapse">
+                            <thead class="bg-gray-100">
+                                <tr>
+                                    <th class="px-4 py-3 text-center text-sm font-semibold text-gray-700">
+                                        <div class="flex items-center justify-center">
+                                            <span>
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5-3.9 19.5m-2.1-19.5-3.9 19.5" />
+                                                </svg>
+                                            </span>
+                                        </div>
+                                    </th>
+                                    <!-- #2 -->
+                                    <th class="px-4 py-3 text-center text-sm font-semibold text-gray-700">
+                                        <div class="flex flex-col items-center gap-1">
+                                            <span class="text-base">Department</span>
+                                            <span>
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 21v-8.25M15.75 21v-8.25M8.25 21v-8.25M3 9l9-6 9 6m-1.5 12V10.332A48.36 48.36 0 0 0 12 9.75c-2.551 0-5.056.2-7.5.582V21M3 21h18M12 6.75h.008v.008H12V6.75Z" />
+                                                </svg>
+                                            </span>
+                                        </div>
+                                    </th>
+                                    <th class="px-4 py-3 text-center text-sm font-semibold text-gray-700">
+                                        <div class="flex flex-col items-center gap-1">
+                                            <span class="text-base">Action</span>
+                                            <span>
+                                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                                                    <path stroke-linecap="round" stroke-linejoin="round" d="M11.42 15.17 17.25 21A2.652 2.652 0 0 0 21 17.25l-5.877-5.877M11.42 15.17l2.496-3.03c.317-.384.74-.626 1.208-.766M11.42 15.17l-4.655 5.653a2.548 2.548 0 1 1-3.586-3.586l6.837-5.63m5.108-.233c.55-.164 1.163-.188 1.743-.14a4.5 4.5 0 0 0 4.486-6.336l-3.276 3.277a3.004 3.004 0 0 1-2.25-2.25l3.276-3.276a4.5 4.5 0 0 0-6.336 4.486c.091 1.076-.071 2.264-.904 2.95l-.102.085m-1.745 1.437L5.909 7.5H4.5L2.25 3.75l1.5-1.5L7.5 4.5v1.409l4.26 4.26m-1.745 1.437 1.745-1.437m6.615 8.206L15.75 15.75M4.867 19.125h.008v.008h-.008v-.008Z" />
+                                                </svg>
+                                            </span>
+                                        </div>
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody class="divide-y divide-gray-200">
+                                <tr
+                                     class="hover:bg-gray-50 transition-colors"
+                                     v-for="(dep,index) in taskInfo?.departments" :key="index"
+                                >
+                                    <td class="px-4 py-3 text-center">{{ index + 1 }}</td>
+                                    <td class="px-4 py-3 text-center">{{ dep['department'].title}}</td>
+                                    <td class="px-4 py-3 flex justify-center">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6 hover:text-red-500 hover:cursor-pointer">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+                                        </svg>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
                     </div>
                     <div v-else-if="flag == 'attachments'">
                         <p>Attachments is clicked</p>
@@ -185,6 +247,6 @@ import Account from '@/views/Account.vue';
                     </div>
                 </div>                
             </div>
-        </div>        
+        </div>    
     </div>
 </template>
